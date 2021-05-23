b2ec13e46fd50506879f8125f00f85516e9a9697
I001user <hung.nt176773@sis.hust.edu.vn>
2021-05-23 07:47:30 +0700

    Revert "add lab-10 unfinished"
    This reverts commit f4bdddfe2804a878b64025c22fc777df5b753a61.

--- Lab10/10.2/login.sql
+++
@@ -1,2 +0,0 @@
-create database loginDB
-use database loginDB
--- Lab10/10.2/loginform.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $linkTo = $_REQUEST["LinkTo"];
-    $username = $_POST["Username"];
-    $password = $_POST["Password"];
-    
-    
-?>
-
