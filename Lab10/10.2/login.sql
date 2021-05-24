ed736d4e8eee4cee24d35019394d7a8bf13d98f8
I001user <hung.nt176773@sis.hust.edu.vn>
2021-05-24 17:21:43 +0700

    another update for lab10

--- Lab10/10.2/login.sql
+++
@@ -1,9 +0,0 @@
-drop table if exists 'account'
-create table 'account'(
-    'username' nvarchar(30) not null,
-    'password' nvarchar(20) not null,
-
-    primary key ('username')
-) engine=InnoDB default charset=utf8
-
-insert into account ('username', 'password') values ('hung', 'admin'), ('quan', 'user');
